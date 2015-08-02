a45b309b78ecc0f7dc21fcbd693d3413c77afa80
Linoa91 <kernmelody@gmail.com>
2015-08-02 18:29:13 +0000

    design mis en place en grande partie

--- public_html/index.php
+++ public_html/index.php
@@ -57,5 +57,56 @@ $app->post('/jouer', function (Request $request) use ($app) {
   return $app['twig']->render('jouer_form.twig.html', array());
 });
 
+/**
+ * @category Controller
+ * Controller de la gallerie de cadeaux
+ */
+$app->get('/cadeaux', function () use ($app) {
+  return $app['twig']->render('cadeaux.twig.html', array(
+    "cadeaux" => array(
+      array(
+        'nom' => '5 voyages à New York',
+        'description' => 'Laissez-vous surprendre par la demesure de "Big Apple", la ville qui ne dort jamais ; une ville environnante qui fascnie par son énergie débordante.',
+        'img' => 'new_york.jpg'
+      ),
+      array(
+        'nom' => '15 weeks end à Paris',
+        'description' => 'Paris est la capitale ;ondiale de la mode, de la culture, de l\'art, de la gastronomie... bref, une destination à ne manquer sous aucun prétexte.',
+        'img' => 'paris.jpg'
+      ),
+      array(
+        'nom' => '50 IPhone 6',
+        'description' => 'Plus large, mais beaucoup plus fin< Plus puissant, mais remarquablement économe en énergie. Sous son design profilé s\'opère une fusion parfaite entre matériel et logiciel.',
+        'img' => 'iphone.jpg'
+      ),
+      array(
+        'nom' => '50 Samsung Galaxy S5',
+        'description' => 'en tant que smartphone propulsé sur le terrain, resserré, du haut de gamme, le GS5, résistant à l\'eau et à la poussière, affiche des caractéristiques techniques haut de gamme.',
+        'img' => 'galaxy_s5.jpg'
+      ),
+      array(
+        'nom' => '1 Suzuki Swift 3 Sport',
+        'description' => 'Le look sportif, c’est avant tout une question d’attitude. Et la Swift n’en manque pas. Homogène, elle offre des vraies sensations de sportive, tout en restant parfaitement civilisée au quotidien.',
+        'img' => 'voiture.jpg'
+      ),
+      array(
+        'nom' => '2 000 places de cinéma UGC',
+        'description' => 'Profiter des meilleurs films du moment avec votre pass solo.',
+        'img' => 'ugc.jpg'
+      ),
+      array(
+        'nom' => '5 000 téléchagements musique',
+        'description' => 'Télécharger votre musique préférée avec votre code promo, c’est gratuit.',
+        'img' => 'musique.jpg'
+      ),
+      array(
+        'nom' => '30 000 goodies',
+        'description' => 'Des milliers de cadeaux aux couleurs de MAC FAST qui vous accompagnerons parout.',
+        'img' => 'goodies.jpg'
+      ),
+    )
+  ));
+});
+
 $app->run();
 ?>
